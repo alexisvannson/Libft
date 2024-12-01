1faf486d23438cb92a175abd9657b0d9944a6327
alexisvannson <vannson.alexis@gmail.com>
2024-12-01 18:33:46 +0100

    presque ok

--- ft_split.c
+++ ft_split.c
@@ -6,54 +6,91 @@
 /*   By: avannson  <marvin@42.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 16:05:17 by avannson          #+#    #+#             */
-/*   Updated: 2024/11/28 12:07:48 by avannson         ###   ########.fr       */
+/*   Updated: 2024/12/01 18:28:17 by avannson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h> 
+#include <stdlib.h>
+#include "libft.h"
 
-int     count_delimiters(char *str, char c)
+#include <stddef.h>
+
+size_t ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
-    int count;
-    int i;
+    size_t src_len;
+    size_t i;
+
+    src_len = 0;
+    while (src[src_len])
+        src_len++;
+
+    if (dstsize == 0)
+        return (src_len);
 
     i = 0;
-    count = 0;
-    while(str[i])
+    while (src[i] && i < dstsize - 1)
     {
-        if (str[i] == c)
-            count++;
+        dst[i] = src[i];
         i++;
+    }
+
+    dst[i] = '\0';
+
+    return (src_len);
+}
+
+int word_count(const char *s, char c)
+{
+    int count = 0;
+    int in_word = 0;
+
+    while (*s)
+    {
+        if (*s == c)
+            in_word = 0;
+        else if (!in_word)
+        {
+            in_word = 1;
+            count++;
+        }
+        s++;
     }
     return (count);
 }
+
+int     get_lenword(char const *str, char c)
+{
+    int len;
+
+    len = 0;
+    while(str[len] && str[len] != c)
+        len++;
+    return (len);
+}
+
 char    **ft_split(char const *s, char c)
 {
-    int nb_delimiters;
     char    **arr;
+    int     len;
     int     i;
-    int k;
-    int j;   
     
-    nb_delimiters = count_delimiters((char*)s, c);
-    arr = malloc(nb_delimiters + 1);
-    j = 0;
-    k = 0;
     i = 0;
-    while (s[i])
+    arr = malloc((word_count(s, c) + 1) * sizeof(char *));
+    if (!arr)
+        return (0);
+    while (*s)
     {
-        if (s[i] == c)
-        {
-            arr[j][k] = '\0';
-            j++;
-            k = 0;
-            continue;
-        }
-        arr[j][k] = s[i];
-        k++;
-        i++;
+        while (*s == c)
+            s++;
+        len = get_lenword(s, c);
+        arr[i] = malloc((len + 1) * sizeof(char*));
+        if (! arr[i])
+            return (0);
+    ft_strlcpy(arr[i], s, len + 1);
+    i++;
+    s += len;
     }
-    arr[j] = 0;
+    arr[i] = 0;
     return (arr);
 }
 /*
@@ -77,4 +114,10 @@ int main(void)
     }
 
     return (0);
-}*/
+}
+*/
+/*Multiple consecutive delimiters
+Delimiters at the start or end of the string
+Proper memory allocation and error handling
+Null-termination of the result array
+*/
